cfa4b4aa24943a9f23a268defda11a89332e97cd
Vitalya <vitalyabes.vb@gmail.com>
2024-02-13 09:48:01 +0300

    Initial commit

---
+++ Container printing.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+#include <list>
+
+template<class T> 
+void print_container(T cont) {
+	auto it = cont.begin();
+
+	while (it != cont.end()) {
+		std::cout << *it << " ";
+		it++;
+	}
+}
+
+int main()
+{
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	print_container<std::set<std::string>>(test_set);
+
+	std::cout << "\n";
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	print_container<std::list<std::string>>(test_list);
+
+	std::cout << "\n";
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };	
+	print_container<std::vector<std::string>>(test_vector);
+	return 0;
+
+}
